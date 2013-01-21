954bf05498f51245ab9e11d07d1dbcb3bd00f353
jtbdevelopment <jtbdevelopment@gmail.com>
2013-01-21 12:26:09 -0500

    finally back to 100% on units

---
+++ E-EYE-O_DAOHibernateImpl/test-unit/com/jtbdevelopment/e_eye_o/hibernate/entities/impl/HibernateAppUserOwnedObjectTest.java
@@ -0,0 +1,70 @@
+package com.jtbdevelopment.e_eye_o.hibernate.entities.impl;
+
+import com.jtbdevelopment.e_eye_o.entities.AppUser;
+import com.jtbdevelopment.e_eye_o.entities.AppUserOwnedObject;
+import com.jtbdevelopment.e_eye_o.entities.IdObjectFactory;
+import com.jtbdevelopment.e_eye_o.entities.Photo;
+import com.jtbdevelopment.e_eye_o.hibernate.entities.wrapper.HibernateIdObjectWrapperFactory;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertSame;
+
+/**
+ * Date: 1/21/13
+ * Time: 11:54 AM
+ */
+public class HibernateAppUserOwnedObjectTest {
+    public static interface LocalInterface extends AppUserOwnedObject {
+    }
+
+    public static class HibernateLocal extends HibernateAppUserOwnedObject<LocalInterface> {
+        public HibernateLocal(final LocalInterface localInterface) {
+            super(localInterface);
+        }
+    }
+
+    private Mockery context;
+    private IdObjectFactory implFactory;
+    private LocalInterface impl;
+    private HibernateLocal hibernateLocal;
+    @SuppressWarnings("unused")  //  used if run standalone to init factory
+    private HibernateIdObjectWrapperFactory daoFactory;
+
+    @BeforeMethod
+    public void setUp() {
+        context = new Mockery();
+        implFactory = context.mock(IdObjectFactory.class);
+        impl = context.mock(LocalInterface.class, "default");
+        daoFactory = new HibernateIdObjectWrapperFactory(implFactory);
+        hibernateLocal = new HibernateLocal(impl);
+        context.checking(new Expectations() {{
+            allowing(implFactory).newIdObject(Photo.class);
+            will(returnValue(impl));
+        }});
+    }
+
+    @Test
+    public void testGetAppUser() throws Exception {
+        final AppUser au = context.mock(AppUser.class);
+        context.checking(new Expectations() {{
+            one(impl).getAppUser();
+            will(returnValue(au));
+        }});
+
+        assertSame(au, hibernateLocal.getAppUser());
+    }
+
+    @Test
+    public void testSetAppUser() throws Exception {
+        final AppUser au = context.mock(AppUser.class);
+        context.checking(new Expectations() {{
+            one(impl).setAppUser(with(any(HibernateAppUser.class)));
+            will(returnValue(impl));
+        }});
+
+        assertSame(hibernateLocal, hibernateLocal.setAppUser(au));
+    }
+}
---
+++ E-EYE-O_DAOHibernateImpl/test-unit/com/jtbdevelopment/e_eye_o/hibernate/entities/impl/HibernateArchivableAppUserOwnedObjectTest.java
@@ -0,0 +1,66 @@
+package com.jtbdevelopment.e_eye_o.hibernate.entities.impl;
+
+import com.jtbdevelopment.e_eye_o.entities.ArchivableAppUserOwnedObject;
+import com.jtbdevelopment.e_eye_o.entities.IdObjectFactory;
+import com.jtbdevelopment.e_eye_o.entities.Photo;
+import com.jtbdevelopment.e_eye_o.hibernate.entities.wrapper.HibernateIdObjectWrapperFactory;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertSame;
+import static org.testng.AssertJUnit.assertTrue;
+
+/**
+ * Date: 1/21/13
+ * Time: 12:22 PM
+ */
+public class HibernateArchivableAppUserOwnedObjectTest {
+    public static interface LocalInterface extends ArchivableAppUserOwnedObject {
+    }
+
+    public static class HibernateLocal extends HibernateArchivableAppUserOwnedObject<LocalInterface> {
+        public HibernateLocal(final LocalInterface localInterface) {
+            super(localInterface);
+        }
+    }
+
+    private Mockery context;
+    private IdObjectFactory implFactory;
+    private LocalInterface impl;
+    private HibernateLocal hibernateLocal;
+    @SuppressWarnings("unused")  //  used if run standalone to init factory
+    private HibernateIdObjectWrapperFactory daoFactory;
+
+    @BeforeMethod
+    public void setUp() {
+        context = new Mockery();
+        implFactory = context.mock(IdObjectFactory.class);
+        impl = context.mock(LocalInterface.class, "default");
+        daoFactory = new HibernateIdObjectWrapperFactory(implFactory);
+        hibernateLocal = new HibernateLocal(impl);
+        context.checking(new Expectations() {{
+            allowing(implFactory).newIdObject(Photo.class);
+            will(returnValue(impl));
+        }});
+    }
+
+    @Test
+    public void testIsArchived() throws Exception {
+        context.checking(new Expectations() {{
+            one(impl).isArchived();
+            will(returnValue(true));
+        }});
+        assertTrue(hibernateLocal.isArchived());
+    }
+
+    @Test
+    public void testSetArchived() throws Exception {
+        context.checking(new Expectations() {{
+            one(impl).setArchived(false);
+            will(returnValue(impl));
+        }});
+        assertSame(hibernateLocal, hibernateLocal.setArchived(false));
+    }
+}
