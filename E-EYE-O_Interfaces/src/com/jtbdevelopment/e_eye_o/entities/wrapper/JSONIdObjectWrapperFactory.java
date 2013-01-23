9737153848644b7edc253dc818e125ecf40ea374
jtbdevelopment <jtbdevelopment@gmail.com>
2013-01-22 21:08:38 -0500

    started jackson json

---
+++ E-EYE-O_Interfaces/src/com/jtbdevelopment/e_eye_o/entities/wrapper/JSONIdObjectWrapperFactory.java
@@ -0,0 +1,10 @@
+package com.jtbdevelopment.e_eye_o.entities.wrapper;
+
+/**
+ * Date: 1/5/13
+ * Time: 8:56 PM
+ * <p/>
+ * Useful as a marker interface
+ */
+public interface JSONIdObjectWrapperFactory extends IdObjectWrapperFactory {
+}
