2f99ad6dc426eb1a4dd48e951e338a8ffe7fbc1e
jtbdevelopment <jtbdevelopment@gmail.com>
2013-01-26 13:07:33 -0500

    placeholder code for jackson

---
+++ E-EYE-O_JSONJacksonImpl/src/com/jtbdevelopment/Placeholder.java
@@ -0,0 +1,11 @@
+package com.jtbdevelopment;
+
+/**
+ * Date: 1/26/13
+ * Time: 1:04 PM
+ */
+public class Placeholder {
+    public Placeholder() {
+        System.out.println("Get rid of me");
+    }
+}
---
+++ E-EYE-O_JSONJacksonImpl/test-unit/com/jtbdevelopment/PlaceholderTest.java
@@ -0,0 +1,16 @@
+package com.jtbdevelopment;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+/**
+ * Date: 1/26/13
+ * Time: 1:04 PM
+ */
+public class PlaceholderTest {
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
