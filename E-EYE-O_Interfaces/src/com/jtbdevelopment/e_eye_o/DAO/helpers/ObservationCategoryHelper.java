e4ce095c48f3930fdeddea392dcd19ca5a2d510a
jtbdevelopment <jtbdevelopment@gmail.com>
2013-01-28 23:00:28 -0500

    added some java doc

--- E-EYE-O_Interfaces/src/com/jtbdevelopment/e_eye_o/DAO/helpers/ObservationCategoryHelper.java
+++ E-EYE-O_Interfaces/src/com/jtbdevelopment/e_eye_o/DAO/helpers/ObservationCategoryHelper.java
@@ -10,6 +10,8 @@ import java.util.Set;
 /**
  * Date: 11/25/12
  * Time: 7:54 PM
+ *
+ * Observation Category Helpers.
  */
 public interface ObservationCategoryHelper {
     public static final Map<String, String> NEW_USER_DEFAULT_CATEGORIES = new HashMap<String, String>() {{
@@ -21,7 +23,19 @@ public interface ObservationCategoryHelper {
         put("IDEA", "Creative");
     }};
 
+    /**
+     * Generates default categories for a user and saves them to DAO
+     *
+     * @param appUser user to create defaults for
+     * @return returns generated and saved values
+     **/
     public Set<ObservationCategory> createDefaultCategoriesForUser(final AppUser appUser);
 
+    /**
+     * Retrieves observation categories from DAO and organizes them into a nice map of short code to OC
+     *
+     * @param appUser user to get OCs for
+     * @return map of OC.shortCode to OC
+     */
     public Map<String, ObservationCategory> getObservationCategoriesAsMap(final AppUser appUser);
 }
