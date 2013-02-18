96d1fc541f767a98bcf00d60adf8c0d005260475
jtbdevelopment <jtbdevelopment@gmail.com>
2013-02-18 18:24:03 -0500

    started jackson units

---
+++ E-EYE-O_SerializationJacksonImpl/test-units/com/jtbdevelopment/e_eye_o/jackson/serialization/JacksonJSONIdObjectSerializerTest.java
@@ -0,0 +1,24 @@
+package com.jtbdevelopment.e_eye_o.jackson.serialization;
+
+import org.testng.annotations.Test;
+
+/**
+ * Date: 2/18/13
+ * Time: 5:25 PM
+ */
+public class JacksonJSONIdObjectSerializerTest {
+    @Test
+    public void testWriteSingleEntities() throws Exception {
+
+    }
+
+    @Test
+    public void testWriteCollection() throws Exception {
+
+    }
+
+    @Test
+    public void testRead() throws Exception {
+
+    }
+}
