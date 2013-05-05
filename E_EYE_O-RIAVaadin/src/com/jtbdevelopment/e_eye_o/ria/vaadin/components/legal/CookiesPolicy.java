96f242fc1bc9edad571eeea2e64b7a4423b35990
jtbdevelopment <jtbdevelopment@gmail.com>
2013-05-05 19:04:55 -0400

    added cookies and privacy policy

--- E_EYE_O-RIAVaadin/src/com/jtbdevelopment/e_eye_o/ria/vaadin/components/legal/CookiesPolicy.java
+++ E_EYE_O-RIAVaadin/src/com/jtbdevelopment/e_eye_o/ria/vaadin/components/legal/CookiesPolicy.java
@@ -6,11 +6,27 @@ import java.math.BigDecimal;
  * Date: 5/2/13
  * Time: 8:45 PM
  */
-//  TODO - text
 public class CookiesPolicy {
     public static final BigDecimal VERSION = new BigDecimal("1.0");
     public static final String TEXT =
             "<H2><CENTER>Credit</H2></CENTER>" +
                     "<p>This document was created using a Contractology template available at http://www.freenetlaw.com." +
-                    "<H2><CENTER>Introduction</H2></CENTER>";
+                    "<H2><CENTER>About cookies</H2></CENTER>" +
+                    "<p>This website uses cookies.  By using this website and agreeing to this policy, you consent to E-EYE-O's use of cookies in accordance with the terms of this policy." +
+                    "<p>Cookies are files sent by web servers to web browsers, and stored by the web browsers. \n" +
+                    "<p>The information is then sent back to the server each time the browser requests a page from the server.  This enables a web server to identify and track web browsers." +
+                    "<p>There are two main kinds of cookies: session cookies and persistent cookies.  Session cookies are deleted from your computer when you close your browser, whereas persistent cookies remain stored on your computer until deleted, or until they reach their expiry date." +
+                    "<H2><CENTER>Cookies on our website</H2></CENTER>" +
+                    "<p>E-EYE-O uses the following cookies on this website, for the following purposes:<ul>" +
+                    "<li>Remember your login (at your discretion) for period of time to avoid logging in each time you use the site." +
+                    "</ul>" +
+                    "<H2><CENTER>Google cookies</H2></CENTER>" +
+                    "<p>E-EYE-O does not yet currently use Google Analytics or Google Adsense, but if we decide to, the following information would be useful.  We will update if and when E-EYE-O uses either of these systems." +
+                    "<p>E-EYO-O uses Google Analytics to analyse the use of this website.  Google Analytics generates statistical and other information about website use by means of cookies, which are stored on users' computers.  The information generated relating to our website is used to create reports about the use of the website. Google will store and use this information.  Google's privacy policy is available at: http://www.google.com/privacypolicy.html." +
+                    "<p>E-EYE-O publishes Google Adsense interest-based advertisements on this website.  These are tailored by Google to reflect your interests.  To determine your interests, Google will track your behaviour across the web using cookies.  You can view, delete or add interest categories associated with your browser using Google's Ads Preference Manager, available at: http://www.google.com/ads/preferences/.  You can opt-out of the Adsense partner network cookie at: http://www.google.com/privacy_ads.html.  However, this opt-out mechanism uses a cookie, and if you clear the cookies from your browser your opt-out will not be maintained. To ensure that an opt-out is maintained in respect of a particular browser, you should use the Google browser plug-in available at: http://www.google.com/ads/preferences/plugin." +
+                    "<H2><CENTER>Refusing cookies</H2></CENTER>" +
+                    "<p>Most browsers allow you to refuse to accept cookies.</H2></CENTER>" +
+                    "<p>In Internet Explorer, you can refuse all cookies by clicking “Tools”, “Internet Options”, “Privacy”, and selecting “Block all cookies” using the sliding selector." +
+                    "<p>In Firefox, you can adjust your cookies settings by clicking “Tools”, “Options” and “Privacy”." +
+                    "<p>Blocking cookies will have a negative impact upon the usability of some websites.";
 }
--- E_EYE_O-RIAVaadin/src/com/jtbdevelopment/e_eye_o/ria/vaadin/components/legal/PrivacyPolicy.java
+++ E_EYE_O-RIAVaadin/src/com/jtbdevelopment/e_eye_o/ria/vaadin/components/legal/PrivacyPolicy.java
@@ -6,11 +6,34 @@ import java.math.BigDecimal;
  * Date: 5/2/13
  * Time: 8:45 PM
  */
-//  TODO - text
 public class PrivacyPolicy {
     public static final BigDecimal VERSION = new BigDecimal("1.0");
     public static final String TEXT =
-            "<H2><CENTER>Credit</H2></CENTER>" +
+            "Your privacy is important to E-EYE-O.  This privacy statement provides information about the personal information that E-EYE-O collects, and the ways in which E-EYE-O uses that personal information." +
+                    "<H2><CENTER>Credit</H2></CENTER>" +
                     "<p>This document was created using a Contractology template available at http://www.freenetlaw.com." +
-                    "<H2><CENTER>Introduction</H2></CENTER>";
+                    "<H2><CENTER>Personal information collection</H2></CENTER>" +
+                    "<p>E-EYE-O may collect and use the following kinds of personal information:<ul>" +
+                    "<li>information about your use of this website;" +
+                    "<li>information that you provide using for the purpose of registering with the website (including email address);" +
+                    "</ul>" +
+                    "<H2><CENTER>Using personal information</H2></CENTER>" +
+                    "<p>E-EYE-O may use your personal information to: <ul>" +
+                    "<li>administer this website;" +
+                    "<li>personalize the website for you;" +
+                    "<li>enable your access to and use of the website services;" +
+                    "</ul>" +
+                    "<p>Where E-EYE-O discloses your personal information to its agents or sub-contractors for these purposes, the agent or sub-contractor in question will be obligated to use that personal information in accordance with the terms of this privacy statement." +
+                    "<p>In addition to the disclosures reasonably necessary for the purposes identified elsewhere above, E-EYE-O may disclose your personal information to the extent that it is required to do so by law, in connection with any legal proceedings or prospective legal proceedings, and in order to establish, exercise or defend its legal rights." +
+                    "<H2><CENTER>Securing your data</H2></CENTER>" +
+                    "<p>E-EYE-O will take reasonable technical and organisational precautions to prevent the loss, misuse or alteration of your personal information." +
+                    "<p>E-EYE-O will store all the personal information you provide on its secure servers." +
+                    "<H2><CENTER>Cross-border data transfers</H2></CENTER>" +
+                    "<p>Information that E-EYE-O collects may be stored and processed in and transferred between any of the countries in which E-EYE-O operates to enable the use of the information in accordance with this privacy policy." +
+                    "<p>You agree to such cross-border transfers of personal information." +
+                    "<H2><CENTER>Updating this statement</H2></CENTER>" +
+                    "<p>E-EYE-O may update this privacy policy by posting a new version on this website." +
+                    "<p>You should check this page occasionally to ensure you are familiar with any changes." +
+                    "<H2><CENTER>Contact E-EYE-O</H2></CENTER>" +
+                    "<p>If you have any questions about this privacy policy or E-EYE-O treatment of your personal information, please use the contact details links on this website.";
 }
