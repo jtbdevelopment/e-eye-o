b854f778933f73afd53560b9dc5ee10bd4e05dd5
JTB <jtbdevelopment@gmail.com>
2013-09-30 20:04:09 -0400

    actual help

--- E-EYE-O_Example-VaadinDerbyJerseyJackson/src/com/jtbdevelopment/e_eye_o/helpandlegal/example/HelpImpl.java
+++ E-EYE-O_Example-VaadinDerbyJerseyJackson/src/com/jtbdevelopment/e_eye_o/helpandlegal/example/HelpImpl.java
@@ -11,16 +11,23 @@ import org.springframework.stereotype.Component;
 @SuppressWarnings("unused")
 public class HelpImpl implements Help {
     private static final String LABEL = "General Help";
-    //  TODO - actual help
     private static final String TEXT =
             "<H2><CENTER>Help</H2></CENTER>" +
-                    "<p>Not implemented yet." +
-                    "<p>Known things to be not working:<ul>" +
-                    "<li>Photos ignore 'How Many' setting." +
-                    "<li>Help not implemented." +
-                    "<li>Changing settings and preferences not implemented." +
-                    "<li>Reports not implemented." +
-                    "</ul>";
+                    "<H3><CENTER>FAQ</CENTER></H3>" +
+                    "" +
+                    "<p><u>Archiving versus Deleting</u>" +
+                    "<p>When you archive something, you simply mark it as inactive.  It will no longer be available on mobile devices and will not show up on reports.  Using the default preferences, it will also be filtered out of your views on the website.  You can re-activate an archived item." +
+                    "<p>When you delete something, it is removed permanently." +
+                    "<p>When you archive or delete something, all related items are generally also archived or deleted. For example, if you archive a student, all observations and photos for that student are also archived or deleted.  Specifically:<ul>" +
+                    "<li>Archiving a student will archive all observations and observation related photos.</li>" +
+                    "<li>Archiving a class list will archive class specific observations and photos as well as all students in the class (and their observations and photos).</li>" +
+                    "<li>Deleting a student will also delete all observations and photos.</li>" +
+                    "<li>Deleting a class list will delete class observations and photos but WILL NOT delete students, they will be updated to no longer be in the class.</li>" +
+                    "<li>Deleting a category will update any observation on that category to no longer be on that category.</li>" +
+                    "</ul>" +
+                    "" +
+                    "<p><u>Suggesting more help topics</u>" +
+                    "<p>Drop us a note using the Contact Us link in the upper right with an area that seems confusing.";
 
     @Override
     public String getLabel() {
