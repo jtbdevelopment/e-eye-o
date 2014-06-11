aa1c2d3e7f608407a1fb92425f646c47484a311c
JTB <jtbdevelopment@gmail.com>
2014-06-10 20:20:48 -0400

    deleting some dirs that came back from merge

--- E-EYE-O_InterfacesMVN/src/test/groovy/com/jtbdevelopment/e_eye_o/entities/wrapper/testinginterfacesandclasses/TestOWFInterfaceSubclass.groovy
+++
@@ -1,9 +0,0 @@
-package com.jtbdevelopment.e_eye_o.entities.wrapper.testinginterfacesandclasses
-
-/**
- * Date: 12/8/13
- * Time: 6:21 PM
- */
-public interface TestOWFInterfaceSubclass extends TestOWFInterface {
-
-}
--- E-EYE-O_InterfacesMVN/src/test/groovy/com/jtbdevelopment/e_eye_o/reflection/testinterfacesandclasses/TestIORInterface2.groovy
+++
@@ -1,8 +0,0 @@
-package com.jtbdevelopment.e_eye_o.reflection.testinterfacesandclasses
-
-/**
- * Date: 12/8/13
- * Time: 9:16 PM
- */
-public interface TestIORInterface2 extends TestIORInterface1 {
-}
